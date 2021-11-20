9a4f626e949c0e880e8e3c70e5064293fd836d96
Pooja Nangude <poojanangude@Poojas-MBP.cable.rcn.com>
2021-11-20 18:58:38 -0500

    first commit

---
+++ services/profile-service.js
@@ -0,0 +1,15 @@
+const profile = require('../data/profile.json');
+
+module.exports = (app) => {
+
+    const getCurrentProfile = (req, res) => {
+        res.json(profile);
+    }
+
+    const updateCurrentProfile = (req, res) => {
+        (res.json(profile));
+    }
+
+    app.get('/api/profile', getCurrentProfile);
+    app.put('/api/profile', updateCurrentProfile);
+};
